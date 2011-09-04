1dc8735379e7b8c5839f88be8ebbe31186212142
Zain Memon <zain@inzain.net>
2011-09-04 12:45:28 -0700

    A crime model with fixtures to load 20k crimes

--- djc/crime/models.py
+++ djc/crime/models.py
@@ -1,10 +1,13 @@
 from django.contrib.gis.db import models
 
 class Crime(models.Model):
-	crime_type = models.CharField(max_length=50)
-	date_time = models.DateTimeField()
-	description = models.TextField()
+    crime_type = models.CharField(max_length=50)
+    date_time = models.DateTimeField()
+    description = models.TextField()
 
-	pt = models.PointField()
+    pt = models.PointField()
 
-	objects = models.GeoManager()
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return "[%s] %s" % (self.crime_type, self.description)
