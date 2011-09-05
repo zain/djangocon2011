6078aaeea716b92f8b36bf63ec1e34d37c51a72b
Zain Memon <zain@inzain.net>
2011-09-05 00:19:56 -0700

    A map that shows crimes

--- djc/crime/views.py
+++ djc/crime/views.py
@@ -1,1 +1,34 @@
-# Create your views here.
+from crime.models import Crime
+from django.contrib.gis.geos import Polygon
+from django.http import HttpResponse
+from django.shortcuts import render
+import datetime, json
+
+
+def map(request):
+    return render(request, 'map.html')
+
+
+def crimes(request):
+    min_lat, min_lon, max_lat, max_lon = request.GET['bbox'].split(',')
+    bbox = Polygon.from_bbox([min_lon, min_lat, max_lon, max_lat])
+
+    crimes = Crime.objects.filter(date_time__gt=datetime.date(2011, 8, 1)).filter(pt__within=bbox)
+
+    fc = {
+        'type': 'FeatureCollection',
+        'features': [{
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [crime.pt.x, crime.pt.y],
+            },
+            'properties': {
+                'crime_type': crime.crime_type,
+                'date_time': crime.date_time.isoformat(),
+                'description': crime.description,
+            }
+        } for crime in crimes],
+    }
+
+    return HttpResponse(json.dumps(fc))
--- djc/urls.py
+++ djc/urls.py
@@ -4,5 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'crime.views.map'),
+    url(r'^crimes$', 'crime.views.crimes'),
     url(r'^admin/', include(admin.site.urls)),
 )
